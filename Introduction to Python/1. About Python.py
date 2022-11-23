7390e11547910fa93677ef006eb887403b5da608
AlzyWelzy <welzyalzy@gmail.com>
2022-11-23 20:08:24 +0530

    doing it

---
+++ Introduction to Python/1. About Python.py
@@ -0,0 +1,30 @@
+# 1. About Python
+"""
+“Python has gotten sufficiently weapons grade that we don’t descend into R anymore. Sorry, R people. I used to be one of you but we no longer descend into R.” – Chris Wiggins
+"""
+
+# 1.1. Overview
+"""
+In this lecture we will
+
+   a. outline what Python is
+
+   b. compare it to some other languages
+
+   c. showcase some of its abilities.
+
+At this stage, it’s not our intention that you try to replicate all you see.
+
+We will work through what follows at a slow pace later in the lecture series.
+
+Our only objective for this lecture is to give you some feel of what Python is, and what it can do.
+"""
+
+# 1.2. What is Python?
+"""
+Python is a general-purpose programming language conceived in 1989 by Dutch programmer Guido van Rossum.
+
+Python is free and open source, with development coordinated through the Python Software Foundation.
+
+Python has experienced rapid adoption in the last decade and is now one of the most popular programming languages.
+"""
