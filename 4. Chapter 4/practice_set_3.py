a8dec9767a941f9a83e6dc9b6003052fc3a5f186
AlzyWelzy <welzyalzy@gmail.com>
2022-12-04 21:18:22 +0530

    this one looks good

---
+++ 4. Chapter 4/practice_set_3.py
@@ -0,0 +1,4 @@
+_tuple = (1, 3, 12, 423, 1234, 12345453, 123412341)
+print(_tuple[0])
+# _tuple[1] = 12  # shows error
+print(_tuple)
---
+++ 4. Chapter 4/practice_set_4.py
@@ -0,0 +1,9 @@
+# total = 0
+sumList = [43214234, 32423, 2341, 12432423]
+# for ele in range(0, len(sumList)):
+#     total = total+sumList[ele]
+
+total = sum(sumList)
+
+
+print(total)
