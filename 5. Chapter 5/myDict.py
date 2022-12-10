ba2523587419b32fcddb43737a14c7bc8d7b465d
AlzyWelzy <welzyalzy@gmail.com>
2022-12-10 13:53:47 +0530

    back to vs code

---
+++ 5. Chapter 5/myDict.py
@@ -0,0 +1,7 @@
+myDict = {
+    "Fast": "In a Quick Manner", "Alzy": "A Programmer", "Marks": [1, 3, 5], "anotherDict": {"Alzy": "Player"}
+}
+
+print(myDict["Fast"])
+print(myDict["Alzy"])
+print(myDict["anotherDict"]["Alzy"])
