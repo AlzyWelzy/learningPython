8e7848b9fdfe7e636a25ea3009aa3076ba3e0af7
AlzyWelzy <welzyalzy@gmail.com>
2022-12-10 18:53:52 +0530

    im doing it

--- 5. Chapter 5/myDict.py
+++ 5. Chapter 5/myDict.py
@@ -1,7 +1,31 @@
+# myDict = {
+#     "Fast": "In a Quick Manner", "Alzy": "A Programmer", "Marks": [1, 3, 5], "anotherDict": {"Alzy": "Player"}
+# }
+
 myDict = {
-    "Fast": "In a Quick Manner", "Alzy": "A Programmer", "Marks": [1, 3, 5], "anotherDict": {"Alzy": "Player"}
+    "fast": "In a Quick Manner",
+    "alzy": "A Programmer",
+    "marks": [1, 3, 5],
+    "anotherDict": {"alzy": "Player"},
+    1: 3
 }
 
-print(myDict["Fast"])
-print(myDict["Alzy"])
-print(myDict["anotherDict"]["Alzy"])
+# print(myDict["Fast"])
+# print(myDict["Alzy"])
+# print(myDict["anotherDict"]["Alzy"])
+# myDict["Marks"] = [45, 78]
+# print(myDict["Marks"])
+# print(myDict["anotherDict"]["Alzy"])
+
+print(myDict.keys())  # Print the keys of the dictionary
+print(myDict.values())  # Print the values of the dictionary
+
+print(type(myDict.keys()))  # Print the type of keys of the dictionary
+print(type(myDict.values()))  # Print the type of values of the dictionary
+
+print(list(myDict.keys()))
+print(list(myDict.values()))
+
+print(myDict[1])
+
+print(myDict.items())
