2e0b9dc1316e04bcef1100a9d12057b73e760545
AlzyWelzy <welzyalzy@gmail.com>
2022-12-12 15:26:07 +0530

    I'm learning about sets

--- 5. Chapter 5/mySets.py
+++ 5. Chapter 5/mySets.py
@@ -1,1 +1,11 @@
-a = {1, 2, 3, 4}
+a = {1, 2, 3, 4, 1, 3, 4, 5, 232, 2, 2, 2, 2}  # Set ignores repetitive words
+print(type(a))
+print(a)
+
+# Important: This syntax will create an empty dictionary instead of an Empty Set
+b = {}
+print(type(b))
+
+# An empty set can be created using below syntax:
+b = set()
+print(type(b))
