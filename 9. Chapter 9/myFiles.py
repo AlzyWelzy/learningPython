a26fd449d17d645a818045db3c6d0e126866b01b
Manvendra Rajpoot <96976159+AlzyWelzy@users.noreply.github.com>
2022-12-15 11:27:06 +0000

    writing it

--- 9. Chapter 9/myFiles.py
+++ 9. Chapter 9/myFiles.py
@@ -7,3 +7,13 @@ text = file.readlines()
 print(text)
 
 file.close()
+
+file = open("9. Chapter 9/example.txt", "a")
+
+file.write("I am writing stuff")
+
+text = file.readlines()
+
+print(text)
+
+file.close()
