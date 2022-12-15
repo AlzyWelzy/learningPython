855ed6c190a312b795ed0ad4a2ff6c2039c4d400
Manvendra Rajpoot <96976159+AlzyWelzy@users.noreply.github.com>
2022-12-15 16:45:18 +0530

    Add files via upload
    working with it

---
+++ 9. Chapter 9/myFiles.py
@@ -0,0 +1,9 @@
+# f = open("9. Chapter 9/example.txt", "r")
+f = open("9. Chapter 9/example.txt")  # by default is "r"
+# data = f.read()
+# data = f.read(2)
+
+# data = f.readline()
+data = f.readlines()
+print(data)
+# f.close()
