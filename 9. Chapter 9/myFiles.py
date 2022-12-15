5d258b672f4ab15d3ad6ae12c73218a69777d422
Manvendra Rajpoot <96976159+AlzyWelzy@users.noreply.github.com>
2022-12-15 11:32:14 +0000

    working with hit

--- 9. Chapter 9/myFiles.py
+++ 9. Chapter 9/myFiles.py
@@ -10,8 +10,24 @@ file.close()
 
 file = open("9. Chapter 9/example.txt", "a")
 
+file.write("I am appending stuff")
+
+# text = file.readlines() # wont work
+
+print(text)
+
+file.close()
+
+file = open("9. Chapter 9/example.txt", "w")  # file will empty
+
 file.write("I am writing stuff")
 
+file.close()
+
+file = open("9. Chapter 9/example.txt", "r")
+# text=file.read()
+
+# text=file.readline()
 text = file.readlines()
 
 print(text)
