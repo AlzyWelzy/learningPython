a6de5c67a22a1ab2bae467b5118b19ac8c266508
AlzyWelzy <welzyalzy@gmail.com>
2022-12-15 19:50:33 +0530

    doing it

--- 9. Chapter 9/practice_4.py
+++ 9. Chapter 9/practice_4.py
@@ -1,3 +1,13 @@
-with open("9. Chapter 9/donkey.txt", "r") as f:
-    a = f.read()
-    print([i for i in range(len(a)) if a.find("DONKEY")])
+# f1 = open("9. Chapter 9/donkey.txt", "r")
+# f2 = open("9. Chapter 9/donkey.txt", "w")
+
+# for word in f1:
+#     f2.write(word.replace("DONKEY", "######"))
+
+# f1.close()
+# f2.close()
+
+with open("9. Chapter 9/donkey.txt") as r:
+    text = r.read().replace("DONKEY", "######")
+with open("9. Chapter 9/donkey.txt", "w") as w:
+    w.write(text)
