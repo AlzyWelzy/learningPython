956493e006f7cfd5f2499d90bf5189260ecdffda
Manvendra Rajpoot <96976159+AlzyWelzy@users.noreply.github.com>
2022-12-15 11:56:31 +0000

    it does

--- 9. Chapter 9/practice_1.py
+++ 9. Chapter 9/practice_1.py
@@ -1,4 +1,18 @@
 with open("9. Chapter 9/poems.txt") as f:
     a = f.read()
 
+
 print(a)
+print(type(a))
+
+print(a.find("twinkle"))
+
+# if (a.find("twinkle")+1):
+#     print("It does")
+# else:
+#     print("it doesn't")
+
+if "twinkle" in a:
+    print("It does")
+else:
+    print("It doesn't")
