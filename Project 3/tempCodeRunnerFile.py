1b452401ceb1fdc1531de2dc1c4a9288f0608ddf
AlzyWelzy <welzyalzy@gmail.com>
2022-12-20 18:15:13 +0530

    removed temp code

--- Project 3/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
- welcomeMsg = '''\n ====== Welcome to Central Library ======
-        Please choose an option:
-        1. List all the books
-        2. Request a book
-        3. Add/Return a book
-        4. Exit the Library
-        '''
-        print(welcomeMsg)
-        a = int(input("Enter a choice: "))
