d1c188d7d2cbcd184e7d981dd87d70fa269a312a
AlzyWelzy <welzyalzy@gmail.com>
2022-12-20 12:50:24 +0530

    made the program

---
+++ Project 3/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+ welcomeMsg = '''\n ====== Welcome to Central Library ======
+        Please choose an option:
+        1. List all the books
+        2. Request a book
+        3. Add/Return a book
+        4. Exit the Library
+        '''
+        print(welcomeMsg)
+        a = int(input("Enter a choice: "))
